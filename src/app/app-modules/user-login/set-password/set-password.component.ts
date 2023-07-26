bd05533d2bdf5e83b03f1ef3bd8a55efd6f2522a
Vaibhav Khanna <va20338741@wipro.com>
2023-07-26 15:09:05 +0530

    Password Encryption and Date Range Validation of Reports

--- src/app/app-modules/user-login/set-password/set-password.component.ts
+++ src/app/app-modules/user-login/set-password/set-password.component.ts
@@ -99,6 +99,7 @@ export class SetPasswordComponent implements OnInit {
   
     generateKey(salt:any, passPhrase:any) {
     return CryptoJS.PBKDF2(passPhrase, CryptoJS.enc.Hex.parse(salt), {
+      hasher: CryptoJS.algo.SHA512,
       keySize: this.keySize / 32,
       iterations: this._iterationCount
     })
